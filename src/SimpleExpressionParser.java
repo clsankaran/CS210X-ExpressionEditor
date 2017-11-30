b9ef0ed517fef5b3efe0114e9f1bdb7e2d330b11
clsankaran <clsankaran@wpi.edu>
2017-11-30 11:09:33 -0500

    Setup

--- src/SimpleExpressionParser.java
+++ src/SimpleExpressionParser.java
@@ -6,6 +6,7 @@
  * X := (E) | L
  * L := [0-9]+ | [a-z]
  */
+
 public class SimpleExpressionParser implements ExpressionParser {
 	/*
 	 * Attempts to create an expression tree -- flattened as much as possible -- from the specified String.
