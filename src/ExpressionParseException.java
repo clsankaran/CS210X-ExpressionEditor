2e026c505dc8ba788520cf7801d84f5ae08abd9a
Charan Sankaran <clsankaran@wpi.edu>
2017-12-03 14:47:35 -0500

    Delete ExpressionParseException.java

--- src/ExpressionParseException.java
+++
@@ -1,8 +0,0 @@
-/**
- * Exception thrown when a ExpressionParser fails to parse a specified string.
- */
-class ExpressionParseException extends Exception {
-	public ExpressionParseException (String message) {
-		super(message);
-	}
-}
