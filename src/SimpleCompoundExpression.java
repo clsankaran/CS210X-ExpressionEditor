f99dc4287d196dc4bbddc235a4c414a1d6303c87
clsankaran <clsankaran@wpi.edu>
2017-12-03 22:22:56 -0500

    Setup All Classes

--- src/SimpleCompoundExpression.java
+++ src/SimpleCompoundExpression.java
@@ -3,21 +3,23 @@ public class SimpleCompoundExpression extends AbstractCompoundExpression {
     // instance variable
     private String _operation;
 
-    public String getOperation (){
-        return _operation;
+    public SimpleCompoundExpression(String operation){
+        _operation = operation;
     }
 
-    public void setOperation (String operation){
-        _operation = operation;
+    public String getOperation (){
+        return _operation;
     }
 
     public void flatten () {
         Boolean sameType = true;
         for(Expression e : this.getChildren()) {
             if (e.getClass() == this.getClass()){
-                if(){
-
+                if(!this._operation.equals(((SimpleCompoundExpression) e).getOperation())){
+                    sameType = false;
                 }
+            } else if (){
+
             }
         }
     }
